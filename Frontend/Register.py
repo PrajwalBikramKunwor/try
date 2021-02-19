9430259bab76a00e6d82a29cd155012711fdec93
PrajwalBikramKunwor <royankunwor43@gmail.com>
2021-02-19 14:02:32 +0545

    third commit

--- Frontend/Register.py
+++ Frontend/Register.py
@@ -1,4 +1,5 @@
 from tkinter import*
+from tkinter import ttk, messagebox
 class Register:
     def __init__ (self,root):
         self.root=root
@@ -10,24 +11,67 @@ class Register:
         
         title= Label(frame1,text="------------------REGISTER TO ON_CART:-------------------", font=("New times roman", 20,"italic"),fg="black",bg="gray").place (x=50,y=30)
 
-
+        #---------------Row1-----------#
+        self.var_fname=StringVar()
         f_name= Label(frame1,text="First name:", font=("New times roman", 15,"bold"),fg="black",bg="gray").place (x=50,y=100)
-        txt_fname= Entry(frame1,font=("times new roman", 15),bg="dark gray").place(x=50,y=130,width = 250)
+        txt_fname= Entry(frame1,font=("times new roman", 15),bg="dark gray",textvariable= self.var_fname).place(x=50,y=130,width = 250)
 
         l_name= Label(frame1,text="Last name:", font=("New times roman", 15,"bold"),fg="black",bg="gray").place (x=370,y=100)
-        txt_lname= Entry(frame1,font=("times new roman", 15),bg="dark gray").place(x=370,y=130,width = 250)
-
+        self.txt_lname= Entry(frame1,font=("times new roman", 15),bg="dark gray")
+        self.txt_lname.place(x=370,y=130,width = 250)
+        #---------------Row2-----------#
         Address= Label(frame1,text="Address:", font=("New times roman", 15,"bold"),fg="black",bg="gray").place (x=50,y=170)
-        txt_address= Entry(frame1,font=("times new roman", 15),bg="dark gray").place(x=50,y=200,width = 250)
+        self.txt_address= Entry(frame1,font=("times new roman", 15),bg="dark gray")
+        self.txt_address.place(x=50,y=200,width = 250)
 
         Age= Label(frame1,text="Age:", font=("New times roman", 15,"bold"),fg="black",bg="gray").place (x=370,y=170)
-        txt_Age= Entry(frame1,font=("times new roman", 15),bg="dark gray").place(x=370,y=200,width = 250)
-
+        self.txt_Age= Entry(frame1,font=("times new roman", 15),bg="dark gray")
+        self.txt_Age.place(x=370,y=200,width = 250)
+        #------------ Row-------------#
         username= Label(frame1,text="Username:", font=("New times roman", 15,"bold"),fg="black",bg="gray").place (x=50,y=240)
-        txt_username= Entry(frame1,font=("times new roman", 15),bg="dark gray").place(x=50,y=270,width = 250)
-
+        self.txt_username= Entry(frame1,font=("times new roman", 15),bg="dark gray")
+        self.txt_username.place(x=50,y=270,width = 250)
         password= Label(frame1,text="Password:", font=("New times roman", 15,"bold"),fg="black",bg="gray").place (x=370,y=240)
-        txt_password= Entry(frame1,font=("times new roman", 15),bg="dark gray").place(x=370,y=270,width = 250)
+        self.txt_password= Entry(frame1,font=("times new roman", 15),bg="dark gray")
+        self.txt_password.place(x=370,y=270,width = 250)
+       
+        email= Label(frame1,text="E-mail:", font=("New times roman", 15,"bold"),fg="black",bg="gray").place (x=50,y=310)
+        self.txt_email= Entry(frame1,font=("times new roman", 15),bg="dark gray")
+        self.txt_email.place(x=50,y=350,width = 250)
+
+        confirm_password= Label(frame1,text="Confirm password:", font=("New times roman", 15,"bold"),fg="black",bg="gray").place (x=370,y=310)
+        self.txt_cpassword= Entry(frame1,font=("times new roman", 15),bg="dark gray")
+        self.txt_cpassword.place(x=370,y=350,width = 250)
+        #---------------------------------------------------------------------------------------------#
+        self.var_chk = IntVar()
+        chk=Checkbutton(frame1,text="I agree to the terms and conditions of the company.",variable=self.var_chk,onvalue=1,offvalue=0,font=("ariel",14,"italic"),bg="gray").place(x=50,y=380)
+
+        btn_register= Button(frame1,text="Register",font=("New times roman",15,"bold"),bg="gray",fg="blue",cursor="hand2", command = self.register_data).place(x=50,y=420, width = 300)
+
+        btn_signin= Button(frame1,text="Sign in",font=("New times roman",15,"bold"),bg="gray",fg="red",cursor="hand2").place(x=370,y=420, width = 300)        
+        #------------------------------------------------------#
+
+    def register_data(self):
+        
+
+        
+        if self.var_fname.get()=="" or self.txt_lname.get()=="" or self.txt_username.get()=="" or self.txt_cpassword.get()==""or self.txt_password.get()=="" or self.txt_Age.get()=="" or self.txt_address.get()=="" or self.txt_email.get()=="":
+        
+            messagebox.showerror("Error","All fields are must!",parent=self.root) 
+        elif self.txt_cpassword.get()!= self.txt_password.get():
+            messagebox.showerror("Wrong password ", "Both the password should match each other",parent= self.root)
+        elif self.var_chk.get()== 0:
+            messagebox.showerror("Error","Please agree to our terms and conditions", parent= self.root)
+        else:
+            messagebox.showinfo("SUCCESS","You are successfully registered to ON_Cart online world")
+        print(self.var_fname.get(), self.txt_lname.get(), self.txt_username.get(), self.txt_password.get(),self.txt_Age.get(),self.txt_address.get(),self.txt_email.get())
+        
+
+
+       
+
+
+
 root=Tk()
 obj = Register(root)
 root.mainloop()
--- Frontend/login.py
+++ Frontend/login.py
@@ -1,4 +1,5 @@
 from tkinter import*
+from tkinter import ttk, messagebox
 class login:
     def __init__(self,root):
         self.root=root
@@ -7,15 +8,32 @@ class login:
         
 
         frame1= Frame(self.root, bg="gray")
-        frame1.place(x=480, y = 100,width=700 ,height=500)
+        frame1.place(x=480, y = 100,width=500 ,height=400)
         
         title=Label(frame1,text="LOGIN TO ON_CART ONLINE WORLD",font=("new times roman",19,"italic"),bg="gray",fg="black").place(x=50,y=30)
 
         username= Label(frame1,text="Username:", font=("New times roman", 16,"bold"),fg="green",bg="gray").place (x=50,y=100)
-        txt_username= Entry(frame1,font=("times new roman", 16),bg="white").place(x=50,y=130,width = 250)
-
+        self.txt_username= Entry(frame1,font=("times new roman", 16),bg="white")
+        self.txt_username.place(x=50,y=130,width = 350)
         password= Label(frame1,text="Password:", font=("New times roman", 16,"bold"),fg="green",bg="gray").place (x=50,y=170)
-        txt_password= Entry(frame1,font=("times new roman", 15),bg="white").place(x=50,y=200,width = 250)
+        self.txt_password= Entry(frame1,font=("times new roman", 15),bg="white")
+        self.txt_password.place(x=50,y=200,width = 350)
+        #------------Buttons department--------------#
+        btn_login= Button(frame1,text="Login",font=("New times roman",15,"bold"),bg="gray",fg="black",cursor="hand2",command= self.login_data).place(x=50,y=240, width = 300)
+
+        btn_register= Button(frame1,text="New to ON_Cart: Register here:",font=("New times roman",15,"bold"),bg="gray",fg="black",cursor="hand2").place(x=50,y=290, width = 300)
+
+        btn_exit= Button(frame1,text="Exit",font=("New times roman",15,"bold"),bg="gray",fg="red",cursor="hand2").place(x=50,y=340, width = 300)
+
+    def login_data(self):
+        if self.txt_username.get()=="" or self.txt_password.get()=="":
+            messagebox.showerror("ERROR", "Fill up both username and password", parent= self.root)
+        print(self.txt_password.get(), self.txt_username.get())
+
+
+
+
+
 
 
 
